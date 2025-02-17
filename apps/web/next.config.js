88a261a209709914c9ab4a3b9a96a689ccac5c52
rgottardi <62321626+rgottardi@users.noreply.github.com>
2025-02-17 11:32:09 -0700

    Add Next.js configuration

---
+++ apps/web/next.config.js
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ['firebasestorage.googleapis.com', 'lh3.googleusercontent.com'],
+  },
+  experimental: {
+    serverActions: true,
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
+}
